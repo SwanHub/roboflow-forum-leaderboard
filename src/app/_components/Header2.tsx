7bd08f2069a29176c18807a6e46e243b347eeba5
SwanHub <jdprince555@gmail.com>
2025-06-11 18:18:30 -0400

    lint

--- src/app/_components/Header2.tsx
+++ src/app/_components/Header2.tsx
@@ -1,11 +1,6 @@
-"use client";
-
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 
 export default function Header2() {
-  const pathname = usePathname();
-
   const navLinks = [
     { href: "https://discuss.roboflow.com/", label: "Forum", isExternal: true },
     { href: "https://app.roboflow.com/", label: "Projects", isExternal: true },
