660eacf885a1b70be7063c13fd67deef5018a860
SwanHub <jdprince555@gmail.com>
2025-06-23 10:31:50 -0400

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Roboflow Forum Leaderboard",
@@ -14,6 +15,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-9780NSMYQN"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-9780NSMYQN');
+          `}
+        </Script>
+      </head>
       <body className="antialiased" style={{ fontFamily: "Arial, sans-serif" }}>
         {children}
       </body>
